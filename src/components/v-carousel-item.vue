e6df37b8b92201afa1ff506da181ccb2e0bb1f7c
yegorich75 <yegorich75@gmail.com>
2021-07-03 11:31:14 +0300

    Element is missing end tag Problem

---
+++ src/components/v-carousel-item.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="v-carousel-item">
+        <img :src="require('../assets/images' + item_data.image + '.jpg') :alt='item_data.alt'>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'v-carousel-item',
+    props: {
+        item_data: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    data() {
+        return {}
+    },
+    computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/v-carousel.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="v-carousel">
+        <v-carousel-item 
+            v-for="item in carousel_data"
+            :key="item.id"
+            :item_data="item"
+        />
+    </div>
+</template>
+
+<script>
+
+import vCarouselItem from './v-carousel-item.vue';
+
+export default {
+    name: "v-carousel",
+    components: {
+        vCarouselItem
+    },
+    props: {
+        carousel_data: {
+            type: Array,
+            default: () => []
+        }
+    },
+    data() {
+        return {}
+    },
+    computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
